4d055da057ef01ccb8e86fee7327f72e353ad166
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-07-02 14:07:08 +0400

    Add booking servlet

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,13 @@
+import controller.BookingServlet;
 import controller.FlightServlet;
+import dao.repository.BookingRepository;
 import dao.repository.FlightRepository;
+import dao.repository.impl.BookingPostgresRepository;
 import dao.repository.impl.FlightPostgresRepository;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import service.impl.BookingServiceImpl;
 import service.impl.FlightServiceImpl;
 
 
@@ -11,8 +15,11 @@ public class App {
     public static void main(String[] args) throws Exception {
         Server server = new Server(9000);
         FlightRepository flightRepository = new FlightPostgresRepository();
+        BookingRepository bookingRepository=new BookingPostgresRepository();
         ServletContextHandler handler    = new ServletContextHandler();
         handler.addServlet(new ServletHolder(new FlightServlet(new FlightServiceImpl(flightRepository))), "/flight/*");
+        handler.addServlet(new ServletHolder(new BookingServlet(new BookingServiceImpl(bookingRepository))), "/booking/*");
+
         server.setHandler(handler);
 
         server.start();
